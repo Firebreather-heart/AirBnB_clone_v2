8d7a19eebc595373e00b357d3286676ce694d39d
firebreather-heart <dtenny95@gmail.com>
2023-09-25 18:39:00 +0100

    all

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -3,7 +3,7 @@
 import os
 from fabric.api import *
 
-env.hosts = ["54.160.85.72", "35.175.132.106"]
+env.hosts = ["100.26.167.169", "100.26.154.174"]
 
 
 def do_clean(number=0):
--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -5,7 +5,7 @@ from fabric.api import env
 from fabric.api import put
 from fabric.api import run
 
-env.hosts = ["54.160.85.72", "35.175.132.106"]
+env.hosts = ["100.26.167.169", "100.26.154.174"]
 
 
 def do_deploy(archive_path):
